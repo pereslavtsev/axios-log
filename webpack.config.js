7ff12a66397b926c4a6230d3f4c89123e90157ae
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2020-04-22 00:27:20 +0300

    feat(fix): fix webpack warning

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  mode: "production",
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
