fcdd7d9b6b1e7888682f9dc0c64f602a52fa7332
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2020-04-26 14:41:49 +0300

    feat(feat): dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const { resolve } = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -7,14 +7,21 @@ module.exports = {
   mode: "production",
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
     libraryTarget: 'commonjs',
   },
   module: {
     rules: [{ test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }],
   },
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    writeToDisk: true
+  },
   resolve: {
-    extensions: ['.ts'],
+    extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"],
+    modules: ['src', 'node_modules']
   },
   plugins: [new CleanWebpackPlugin()],
 };
